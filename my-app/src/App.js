20ad13f70ce8fd92fe9f61cc4c0d2d40c2b41990
ASHISH PAL <143118293+ashishpal9@users.noreply.github.com>
2024-05-17 13:47:08 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hii my name is ashish pal
+          hii my name is ashish pal .....
         </p>
         <a
           className="App-link"
